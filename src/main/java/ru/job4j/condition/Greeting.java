fb1595b4738934ec4188999f8660e58cceb93009
TimashovDmitry <timashov_dmitry@mail.ru>
2023-06-29 12:06:55 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java";
+        System.out.println(idea);
+        System.out.println(idea + " " + "But I am a newbie");
+        int year = 2023;
+        idea = idea + " " + year;
+        System.out.println(idea);
+    }
+}
