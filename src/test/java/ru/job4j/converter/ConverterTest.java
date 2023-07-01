bc3ccb1ba70b06dadd3c5206454256e255a75c0c
TimashovDmitry <timashov_dmitry@mail.ru>
2023-07-01 18:40:31 +0300

    1. Модульные тесты. [#159435 #426645]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,9 +15,9 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert200RblThen3dot3333Dlr() {
-        float in = 240;
-        float expected = 4;
+    public void whenConvert180RblThen3Dlr() {
+        float in = 180;
+        float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
