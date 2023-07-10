6fcbf21b3061f659000d8994ebf97a7951566bf6
TimashovDmitry <timashov_dmitry@mail.ru>
2023-07-10 18:28:21 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[10];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Dmitry Timashov";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Andrey Petrov";
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
-
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
