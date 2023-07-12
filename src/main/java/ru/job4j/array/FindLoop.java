af40fc17989793f53840ecd204b1a3ece4ba6a51
TimashovDmitry <timashov_dmitry@mail.ru>
2023-07-13 00:59:34 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,16 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rls = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rls = i;
+                break;
+            }
+        }
+        return rls;
+    }
 }
+
